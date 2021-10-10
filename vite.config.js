a778fa39936f56e92a8ed67f6cf6cdae2075832a
Nathan <nathanckim18@gmail.com>
2021-10-10 13:11:09 -0400

    hacky fixes to get yarn build  + reveal to work nicely together

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import {defineConfig} from 'vite'
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: [
-        'node_modules/reveal.js/dist/reveal.css',
-        'node_modules/reveal.js/dist/theme/moon.css',
-        'ui/styles/hjs-solarized-light.css',
-      ],
+      external: ['ui/slides.md'],
     },
   },
 })
